b68ffa88a135ae9127ff699979d7fced31d72957
_nebula <41904486+misternebula@users.noreply.github.com>
2024-01-11 11:41:18 +0000

    Update DisplayView.java

--- clients/shopDisplay/DisplayView.java
+++ clients/shopDisplay/DisplayView.java
@@ -21,11 +21,12 @@ import java.util.Observer;
 public class DisplayView extends Canvas implements Observer
 {
   private static final long serialVersionUID = 1L;
-  private Font font = new Font("Monospaced",Font.BOLD,24);
-  private int H = 400;         // Height of window
-  private int W = 600;         // Width  of window
-  private String textToDisplay = "";
-  private DisplayController cont= null;
+  private int H = 400;
+  private int W = 600;
+  private DisplayController cont = null;
+  private final JTextArea waitingTextArea;
+  private final JTextArea beingPickedTextArea;
+  private final JTextArea readyTextArea;
   
   /**
    * Construct the view
@@ -37,12 +38,48 @@ public class DisplayView extends Canvas implements Observer
   
   public DisplayView(  RootPaneContainer rpc, MiddleFactory mf, int x, int y )
   {
-    Container cp         = rpc.getContentPane();    // Content Pane
+    Container cp = rpc.getContentPane();    // Content Pane
     Container rootWindow = (Container) rpc;         // Root Window
-    cp.setLayout( new BorderLayout() );             // Border N E S W CENTER 
-    rootWindow.setSize( W, H );                     // Size of Window  
-    rootWindow.setLocation( x, y );                 // Position on screen
-    rootWindow.add( this, BorderLayout.CENTER );    //  Add to rootwindow
+    cp.setLayout(null);                             // No layout manager
+    rootWindow.setSize( W, H );                     // Size of Window
+    rootWindow.setLocation( x, y );
+
+    var waitingLabel = new JLabel("Waiting");
+    Font font = new Font("Monospaced", Font.BOLD, 24);
+    waitingLabel.setFont(font);
+    waitingLabel.setBounds(30, 10, 180, 30);
+    waitingLabel.setHorizontalAlignment(JLabel.CENTER);
+    cp.add(waitingLabel);
+
+    waitingTextArea = new JTextArea();
+    waitingTextArea.setBounds(30, 40, 180, 300);
+    waitingTextArea.setFont(font);
+    waitingTextArea.setEditable(false);
+    cp.add(waitingTextArea);
+
+    var beingPickedLabel = new JLabel("Being Picked");
+    beingPickedLabel.setFont(font);
+    beingPickedLabel.setBounds(210, 10, 180, 30);
+    beingPickedLabel.setHorizontalAlignment(JLabel.CENTER);
+    cp.add(beingPickedLabel);
+
+    beingPickedTextArea = new JTextArea();
+    beingPickedTextArea.setBounds(210, 40, 180, 300);
+    beingPickedTextArea.setFont(font);
+    beingPickedTextArea.setEditable(false);
+    cp.add(beingPickedTextArea);
+
+    var readyLabel = new JLabel("Ready");
+    readyLabel.setFont(font);
+    readyLabel.setBounds(390, 10, 180, 30);
+    readyLabel.setHorizontalAlignment(JLabel.CENTER);
+    cp.add(readyLabel);
+
+    readyTextArea = new JTextArea();
+    readyTextArea.setBounds(390, 40, 180, 300);
+    readyTextArea.setFont(font);
+    readyTextArea.setEditable(false);
+    cp.add(readyTextArea);
     
     rootWindow.setVisible( true );                  // Make visible
   }
@@ -70,78 +107,15 @@ public class DisplayView extends Canvas implements Observer
       Map<String, List<Integer> > res =
       ( (DisplayModel) aModelOfDisplay ).getOrderState();
 
-      textToDisplay = 
-           "Orders in system" + "\n" +
-           "Waiting        : " + listOfOrders( res, "Waiting" ) + 
-           "\n"  + 
-           "Being picked   : " + listOfOrders( res, "BeingPicked" ) + 
-           "\n"  + 
-           "To Be Collected: " + listOfOrders( res, "ToBeCollected" );
+      waitingTextArea.setText(listOfOrders(res, "Waiting"));
+      beingPickedTextArea.setText(listOfOrders(res, "BeingPicked"));
+      readyTextArea.setText(listOfOrders(res, "ToBeCollected"));
     }
-    catch ( OrderException err )
-    {
-      textToDisplay = "\n" + "** Communication Failure **";
+    catch ( OrderException err ) {
+      throw new RuntimeException(err);
     }
-    repaint();                            // Draw graphically    
-  }
-  
-  @Override
-  public void update( Graphics g )        // Called by repaint
-  {                                       // 
-    drawScreen( (Graphics2D) g );         // Draw information on screen
-  }
 
-    /**
-     * Redraw the screen double buffered
-     * @param g Graphics context
-     */
-  @Override 
-  public void paint( Graphics g )         // When 'Window' is first 
-  {                                       //  shown or damaged 
-    drawScreen( (Graphics2D) g );         // Draw information on screen
-  }
-
-  private Dimension     theAD;           // Alternate Dimension
-  private BufferedImage theAI;           // Alternate Image
-  private Graphics2D    theAG;           // Alternate Graphics
-  
-  public void drawScreen( Graphics2D g )  // Re draw contents 
-  {                                         //  allow resize
-    Dimension d    = getSize();             // Size of image
-
-    if (  ( theAG == null )           ||
-          ( d.width  != theAD.width ) ||
-          ( d.height != theAD.height )   )
-    {                                       // New size
-      theAD = d;
-      theAI = (BufferedImage) createImage( d.width, d.height );
-      theAG = theAI.createGraphics();
-    }
-    drawActualScreen( theAG );            // draw
-    g.drawImage( theAI, 0, 0, this );     // Now on screen
-  }
-  
-  /**
-   * Redraw the screen
-   * @param g Graphics context
-   */
- 
-  public void drawActualScreen( Graphics2D g )  // Re draw contents 
-  {
-    g.setPaint( Color.white );            // Paint Colour 
-    W = getWidth(); H = getHeight();      // Current size
-    
-    g.setFont( font );
-    g.fill( new Rectangle2D.Double( 0, 0, W, H ) );
-
-    // Draw state of system on display
-    String lines[] = textToDisplay.split("\n");
-    g.setPaint( Color.black );
-    for ( int i=0; i<lines.length; i++ )
-    {
-      g.drawString( lines[i], 0, 50 + 50*i );
-    }
-    
+    repaint();
   }
 
   /**
@@ -152,17 +126,26 @@ public class DisplayView extends Canvas implements Observer
    */
   private String listOfOrders( Map<String, List<Integer> > map, String key )
   {
-    String res = "";
+    StringBuilder res = new StringBuilder();
+
     if ( map.containsKey( key ))
     {
       List<Integer> orders = map.get(key);
-      for ( Integer i : orders )
+
+      for (var i = 0; i < orders.size(); i++)
       {
-        res += " " + i;
+        if (i == 0)
+        {
+          res.append(orders.get(i));
+        }
+        else{
+          res.append("\r\n").append(orders.get(i));
+        }
       }
     } else {
-      res = "-No key-";
+      res = new StringBuilder("-No key-");
     }
-    return res;
+
+    return res.toString();
   }
 }
